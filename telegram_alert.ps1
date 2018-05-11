768b0a77cbe12104bc8896bfd1a07bfec2fd0d7a
A.Solovey <solovey.anton@gmail.com>
2018-05-11 17:37:41 +0300

    sync telegram script with master

--- telegram_alert.ps1
+++ telegram_alert.ps1
@@ -3,25 +3,35 @@
    Created by Anton Solovey @ Falcongaze LLC, 2018
 #>
 
-#$token = "token"
-#$chat_id = "chat_id"
-$token = "476433888:AAGpKv8mSX8T5INoH8kAFVdqLTNXR02Snmo"
-$chat_id = "34444503"
-$text = "SecureTower alert `
-	Rule: $env:SEARCHRULENAME at $env:ALERTTIME. `
-	Link: $env:DOCUMENTLINK"
+#   Due to RKN is bloking Telegram messenger in Russia you have to use proxy server
+#   You can find a lof of free servers at https://hidemy.name
 
+$Proxy = "proxy_server_ip:port"
+
+$token = "token"
+$chat_id = "chat_id"
+
+$text = "*SecureTower alert* `
+
+	Rule: $env:FGST_SEARCH_RULE_ID at $env:FGST_ALERT_TIME `
+    Link: $env:FGST_DOCUMENT_LINK `
+
+	*Details* `
+
+	*User*: $env:FGST_USER_LOCAL_NAME `
+	*IP*: $env:FGST_MACHINE_ADDRESS_IP `
+	*SID*: $env:FGST_USER_SID"
 
 $payload = @{
     "chat_id" = $chat_id;
-    "text" = $text
+    "text" = $text;
+    "parse_mode" = "Markdown";
+    "disable_web_page_preview" = "True"
 }
 
-$res = Invoke-WebRequest `
+Invoke-WebRequest `
     -Uri ("https://api.telegram.org/bot{0}/sendMessage" -f $token) `
     -Method Post `
     -ContentType "application/json;charset=utf-8" `
-    -Body (ConvertTo-Json -Compress -InputObject $payload)
-
-Write-host "Status Code: "  $res.StatusCode
-Write-host $res.Content
+    -Body (ConvertTo-Json -Compress -InputObject $payload) `
+    -Proxy $Proxy
