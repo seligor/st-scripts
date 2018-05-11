56ecae2dad5c5a713df20271ae9a67023eb03ba1
A.Solovey <solovey.anton@gmail.com>
2018-05-11 17:29:31 +0300

    new scripts init

--- telegram_alert.ps1
+++ telegram_alert.ps1
@@ -3,8 +3,10 @@
    Created by Anton Solovey @ Falcongaze LLC, 2018
 #>
 
-$token = "token"
-$chat_id = "chat_id"
+#$token = "token"
+#$chat_id = "chat_id"
+$token = "476433888:AAGpKv8mSX8T5INoH8kAFVdqLTNXR02Snmo"
+$chat_id = "34444503"
 $text = "SecureTower alert `
 	Rule: $env:SEARCHRULENAME at $env:ALERTTIME. `
 	Link: $env:DOCUMENTLINK"
@@ -15,8 +17,11 @@ $payload = @{
     "text" = $text
 }
 
-Invoke-WebRequest `
+$res = Invoke-WebRequest `
     -Uri ("https://api.telegram.org/bot{0}/sendMessage" -f $token) `
     -Method Post `
     -ContentType "application/json;charset=utf-8" `
     -Body (ConvertTo-Json -Compress -InputObject $payload)
+
+Write-host "Status Code: "  $res.StatusCode
+Write-host $res.Content
