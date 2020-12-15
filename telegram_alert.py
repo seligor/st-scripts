d3889cef2256513c377d43b3d3997253ade82d6f
Igor Baluev <s-el-igor@mail.ru>
2020-12-15 14:15:31 +0500

    Update telegram_alert.py

--- telegram_alert.py
+++ telegram_alert.py
@@ -10,12 +10,13 @@ machineip=(os.environ['FGST_MACHINE_ADDRESS_IP'])
 sid=(os.environ['FGST_USER_SID'])
 
 
+documentlink = documentlink.rstrip(';')
 
 api_token = '$TOKEN$'
 
 requests.get('https://api.telegram.org/bot{}/sendMessage'.format(api_token), params=dict(
     chat_id='$CHATID$',
-text='SecureTower alert' + '\n\r' + searchrulename + '\n\r' + documentlink + '\n\r' + alerttime + '\n\r\n\r' + username + '\n\r' + sid + '\n\r' + machineip
+text='SecureTower alert  ' + searchrulename + '\n\r' + documentlink + '\n\r' + alerttime + '\n\r\n\r' + username + '\n\r' + sid + '\n\r' + machineip
    
 
 ))
